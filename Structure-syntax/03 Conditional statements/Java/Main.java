74c40070885979a33dfb1d413be987dffa17b5bc
mediascan <113550998+mediascan@users.noreply.github.com>
2022-11-19 17:37:03 +0300

    end lection
    end lection

--- Structure-syntax/03 Conditional statements/Java/Main.java
+++ Structure-syntax/03 Conditional statements/Java/Main.java
@@ -5,7 +5,7 @@ public class Main {
 public static void main(String [] argv)
   {
     // Variable declarations
-    int Variable = 0;
+    int Variable = 44;
     if (Variable > 0)
        System.out.println (" > 0 ");
     else if (Variable < 0)
--- Structure-syntax/06 Cycles/C++/obj/main.d
+++ Structure-syntax/06 Cycles/C++/obj/main.d
@@ -1,1 +1,1 @@
-main.o: L:\projOOP\Structure-syntax\06\ Cycles\C++\src\main.cpp 20221110181740
+main.o: D:\GitHub\BSAC-IT161-OOP\Structure-syntax\06\ Cycles\C++\src\main.cpp 20221119083648
