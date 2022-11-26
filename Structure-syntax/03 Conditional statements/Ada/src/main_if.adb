de62a9f98328580820f27b3916feabb11f308f26
mediascan <113550998+mediascan@users.noreply.github.com>
2022-11-26 11:57:10 +0300

    App
    appp code

--- Structure-syntax/03 Conditional statements/Ada/src/main_if.adb
+++ Structure-syntax/03 Conditional statements/Ada/src/main_if.adb
@@ -15,3 +15,20 @@ begin
       Put_Line (" = 0 ");
    end if;
 end Main_if;
+--  --
+--  -- Ada program
+--  --
+--  with Ada.Text_IO;
+--  use Ada.Text_IO;
+--  procedure Main_if is
+--     -- Variable declarations
+--     Variable : Integer := 4;
+--  begin
+--     if Variable > 0 then
+--        Put_Line (" > 0 ");
+--     elsif Variable < 0 then
+--        Put_Line (" < 0 ");
+--     else
+--        Put_Line (" = 0 ");
+--     end if;
+--  end Main_if;
